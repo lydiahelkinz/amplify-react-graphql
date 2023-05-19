8e0fc4c40bb5dc0aae15d6e8024cd0d6ebad4fb1
lydia lebdiri <lydia.lebdiri@helkinz.com>
2023-05-19 15:15:52 +0200

    react app auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplifyreactgraphqlfe30a7c7": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,54 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-import { useEffect, useState } from 'react';
-import Amplify from '@aws-amplify/core';
-import { Storage } from 'aws-amplify';
+import logo from "./logo.svg";
+import "@aws-amplify/ui-react/styles.css";
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
 
-
-function App() {
-  const [imageUrl, setImageUrl] = useState(null);
-
-  useEffect(() => {
-    Amplify.configure({
-      Auth: {
-        identityPoolId: "eu-west-3:8fc6c550-1501-4170-ad38-3a74454b55e1",
-        region: "eu-west-3",
-      },
-      Storage: {
-        AWSS3: {
-          bucket: "react-bucket-lydia",
-          region: "eu-west-3",
-        },
-      },
-    });
-    loadImage();
-  }, []);
-
-  const loadImage = () => {
-    Storage.get("data.jpg")
-      .then((result) => {
-        setImageUrl(result);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
+function App({ signOut }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>React AWS Storage</h1>
-        {imageUrl && (
-          <img
-            src={imageUrl}
-            alt="Data"
-            style={{ width: "500px", height: "500px" }}
-          />
-        )} 
-      </header>
-    </div>
+    <View className="App">
+      <Card>
+        <Image src={logo} className="App-logo" alt="logo" />
+        <Heading level={1}>We now have Auth!</Heading>
+      </Card>
+      <Button onClick={signOut}>Sign Out</Button>
+    </View>
   );
 }
 
-export default App;
-
+export default withAuthenticator(App);
